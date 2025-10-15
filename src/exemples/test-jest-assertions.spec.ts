75226adcb39e97be73ff5ab3eac2f53dc6b6801f
AlvesKZ <igoralvex01@gmail.com>
2025-10-15 18:33:03 -0300

    Testando Messaging.ts

--- src/exemples/test-jest-assertions.spec.ts
+++ src/exemples/test-jest-assertions.spec.ts
@@ -1,7 +1,7 @@
 describe('Primitive values', () => {
     it('should test jest assertions', () => {
        const number = 10;
-       
+
        expect(number).toBe(10);
        expect(number).toEqual(10);
 
@@ -22,7 +22,15 @@ describe('Primitive values', () => {
 
 describe('Objects', () => {
     it('should test jest assertions with objects', () => {
-        
+        const person = { name: 'Luiz', age: 30 };
+        const anotherPerson = { ...person };
+
+        expect(person).toEqual(anotherPerson);
+        expect(person).toHaveProperty('age');
+        expect(person).not.toHaveProperty('lastName');
+        expect(person).toHaveProperty('age', 30);
+
+        expect(person.name).toBe('Luiz');
     });
 });
 
